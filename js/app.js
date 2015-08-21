17b653af116b98045042d26a479bdfaba28dec81
amanbis <amanb91@gmail.com>
2015-08-21 17:50:26 +0700

    Try hide instead of detach for modal content
    This is to get fadeIn to initiate.

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@ $(document).ready(function(){
             type: 'GET',
             dataType: 'html',
             success: function(result) {
-                $('.modalContent').detach().html(result).appendTo('.modalDialog').fadeIn(4000);
+                $('.modalContent').hide().html(result).appendTo('.modalDialog').fadeIn('slow');
             },
             error: function(request, errorType, errorMessage) {
                 alert('Error: ' + errorType + ' with message: ' + errorMessage);
